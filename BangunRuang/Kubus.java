f0888bbe387f8883a001faa653a70b16c6f0e65f
AgungGumelar <115860949+gnuga1@users.noreply.github.com>
2022-11-20 15:04:05 +0700

    Add files via upload

---
+++ BangunRuang/Kubus.java
@@ -0,0 +1,18 @@
+package BangunRuang;
+
+public class Kubus extends Kotak {
+    public Kubus(double sisi) {
+        super(sisi);
+    }
+
+    @Override
+    public void volume() {
+        this.volume = Math.pow(sisi, 3);
+    }
+
+    @Override
+    public void luasPermukaan() {
+        this.luasPermukaan = 6 * Math.pow(sisi, 2);
+    }
+}
+Footer
